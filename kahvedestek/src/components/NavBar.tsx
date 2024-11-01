19e4033b1446f659d414861c05686cdac0fff9f1
anaktas24 <johnantony24@hotmail.com>
2024-11-01 14:17:44 +0100

    pages and navbar structure

--- kahvedestek/src/components/NavBar.tsx
+++ kahvedestek/src/components/NavBar.tsx
@@ -6,8 +6,12 @@ const NavBar = () =>{
 return(
     <div className='flex items-center justify-between py-5 font-medium'>
         {/* <img src={assets.logo} classname='w-36' alt="logo" */}
-
-        
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+            <NavLink to='/'className='flex flex-col items-center gap-1'>
+                <p>Home</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700'/>
+            </NavLink>
+        </ul>
     </div>
 )
 }
