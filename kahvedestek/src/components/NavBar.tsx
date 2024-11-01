dcfc65f27c5bf2e787e0309db486f3782401efb8
anaktas24 <johnantony24@hotmail.com>
2024-11-01 14:00:03 +0100

    pages and navbar structure

---
+++ kahvedestek/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+//import {assets} from '../assets/assets'
+
+const NavBar = () =>{
+return(
+    <div className='flex items-center justify-between py-5 font-medium'>
+        {/* <img src={assets.logo} classname='w-36' alt="logo" */}
+
+        
+    </div>
+)
+}
+
+export default NavBar
