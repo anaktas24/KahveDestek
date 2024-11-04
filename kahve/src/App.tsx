c52594219515f7f01f2e44eed97a150fcdea5ecf
anaktas24 <johnantony24@hotmail.com>
2024-11-04 09:13:47 +0100

    navbar and responsive

--- kahve/src/App.tsx
+++ kahve/src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Contact from "./pages/Contact"
+import Login from "./pages/Login"
+import PlaceOrder from "./pages/PlaceOrder"
+import Orders from "./pages/Orders"
+import NavBar from "./components/NavBar"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/product/:productId" element={<Product/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/collection" element={<Collection/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/place-order" element={<PlaceOrder/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+      </Routes>
+    </div>
+    
   )
 }
 
