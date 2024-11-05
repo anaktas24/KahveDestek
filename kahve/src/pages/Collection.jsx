53cdbce115920e705d5217522a21326b81acd5a8
anaktas24 <johnantony24@hotmail.com>
2024-11-05 13:03:36 +0100

    Sorting, Filter

--- kahve/src/pages/Collection.jsx
+++ kahve/src/pages/Collection.jsx
@@ -1,12 +1,73 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets/assets';
+import ProductItem from '../components/ProductItem';
+import Title from '../components/Title';
 
 const Collection = () => {
 
   const {products} = useContext(ShopContext)
   const [showFilter,setShowFilter] = useState(false)
-  console.log(products);
+  const [filterProducts,setFilterProducts] =useState([])
+  const [category,setCategory] = useState([])
+  const [subCategory,setSubCategory] = useState([])
+  const [sortType,setSortType] = useState('relevant')
+
+  const toggleCategory = (e)=>{
+    if (category.includes(e.target.value)) {
+      setCategory(prev=> prev.filter(item => item != e.target.value))
+    } else {
+      setCategory(prev=> [...prev, e.target.value])
+    }
+  }
+  const toggleSubCategory = (e)=>{
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory(prev => prev.filter(item => item != e.target.value))
+    } else {
+      setSubCategory(prev=>[...prev,e.target.value])
+    }
+  }
+
+  const applyFilter= ()=>{
+    let productsCopy = products.slice()
+    if (category.lenght>0) {
+      productsCopy = productsCopy.filter(item=>category.includes(item.category))
+    } 
+    if (subCategory.lenght>0) {
+      productsCopy = productsCopy.filter(item=>subCategory.includes(item.subCategory))
+    }
+    setFilterProducts(productsCopy)
+  }
+
+  const sortProduct = ()=>{
+    let fpCopy = filterProducts.slice()
+    switch (sortType){
+      case 'low-high':
+        setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)))
+        break;
+      case 'high-low':
+        setFilterProducts(fpCopy.sort((a,b)=>(b.price - a.price)))
+        break;
+      default:
+        applyFilter()
+        break;
+
+    }
+  }
+
+  useEffect(()=>{
+    applyFilter()
+  },[category,subCategory])
+
+  useEffect(()=>{
+    sortProduct()
+  },[sortType])
+
+
+
+
+
+
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
 
@@ -21,16 +82,16 @@ const Collection = () => {
             <p className="mb-3 text-sm font-medium">CATEGORIES</p>
             <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'filtervalue'} />Filter 1
+                <input type="checkbox" className="w-3" value={'filtervalue'} onChange={toggleCategory}  />Filter 1
               </p>
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'filtervalue'} />Filter 2
+                <input type="checkbox" className="w-3" value={'filtervalue'} onChange={toggleCategory} />Filter 2
               </p>
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'filtervalue'} />Filter 3
+                <input type="checkbox" className="w-3" value={'filtervalue'} onChange={toggleCategory} />Filter 3
               </p>
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'filtervalue'} />Filter 4
+                <input type="checkbox" className="w-3" value={'filtervalue'} onChange={toggleCategory} />Filter 4
               </p>
             </div>
           </div>
@@ -40,16 +101,16 @@ const Collection = () => {
             <p className="mb-3 text-sm font-medium">TYPE</p>
             <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} />subcategoryFilter 1
+                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} onChange={toggleSubCategory} />subcategoryFilter 1
               </p>
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} />subcategoryFilter 2
+                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} onChange={toggleSubCategory} />subcategoryFilter 2
               </p>
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} />subcategoryFilter 3
+                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} onChange={toggleSubCategory} />subcategoryFilter 3
               </p>
               <p className="flex gap-2">
-                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} />subcategoryFilter 4
+                <input type="checkbox" className="w-3" value={'subcategoryfiltervalue'} onChange={toggleSubCategory} />subcategoryFilter 4
               </p>
             </div>
           </div>
@@ -61,7 +122,7 @@ const Collection = () => {
           <div className="flex justify-between text-base sm:text-2xl mb-4">
             <Title text1={'ALL'} text2={'COLLECTIONS'}/>
             {/*Sort by */}
-            <select className='border-2 border-gray-300 text-sm px-2'>
+            <select onChange={(e)=>setSortType(e.target.value)}className='border-2 border-gray-300 text-sm px-2'>
               <option value="relevant">Sort by: Relevance</option>
               <option value="low-high">Sort by: Low to High</option>
               <option value="high-low">Sort by: High to Low</option>
@@ -69,6 +130,13 @@ const Collection = () => {
           </div>
 
           {/*Map Products */}
+          <div className="grid grid-cols-2 md:grid-cols3 lg:grid-cols-4 gap-4 gap-y-6">
+            {
+            filterProducts.map((item,index)=>(
+              <ProductItem key={'value'} id={item.id} name={item.name} price={item.price} image={item.image}/>
+            ))
+            }
+          </div>
         </div>
       </div>
   )
