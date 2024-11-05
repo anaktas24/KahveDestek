7bf035dc7b0aba25906f6fd4348901b334adb061
anaktas24 <johnantony24@hotmail.com>
2024-11-05 09:05:50 +0100

    Footer,Newsletter

--- kahve/src/pages/Home.tsx
+++ kahve/src/pages/Home.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
+import NewsletterBox from '../components/NewsletterBox'
+import OurPolicy from '../components/OurPolicy'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LatestCollection/>
+      <OurPolicy/>
+      <NewsletterBox/>
+      <Footer/>
     </div>
   )
 }
