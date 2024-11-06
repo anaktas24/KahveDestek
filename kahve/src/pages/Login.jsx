2bd3eb9de1eac794e1b23becfb2d03137686526c
anaktas24 <johnantony24@hotmail.com>
2024-11-06 13:27:13 +0100

    Contac/About pages

--- kahve/src/pages/Login.jsx
+++ kahve/src/pages/Login.jsx
@@ -4,9 +4,13 @@ const Login = () => {
 
   const [currentState,setCurrentState] = useState('Sign Up')
 
+  const onSubmitHandler=async(event)=>{
+    event.prevendDefault()
+  }
+
 
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+    <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="poppins-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 text-gray-800" />
@@ -22,6 +26,7 @@ const Login = () => {
           :<p onClick={()=>setCurrentState('Login')}className="curso-pointer">Login</p>
         }
       </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
     </form>
     
   )
