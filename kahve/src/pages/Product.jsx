07d8c8a0441cc49cb97eab07fea2f7a2c3a695a8
anaktas24 <johnantony24@hotmail.com>
2024-11-06 11:34:12 +0100

    login/signup

--- kahve/src/pages/Product.jsx
+++ kahve/src/pages/Product.jsx
@@ -32,11 +32,17 @@ const Product = () => {
         {/*------Product Images------- */}
         <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full ">
-            {
-              productData.image.map((item,index)=>(
-                <img onClick={()=>setImage(item)} src={item} alt="" className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" />
-              ))
-            }
+          {
+            productData.image.map((item, index) => (
+              <img 
+                key={index} 
+                onClick={() => setImage(item)} 
+                src={item} 
+                alt="" 
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" 
+              />
+            ))
+}
           </div>
           <div className="w-full sm:w-[80%]">
             <img src={image} className='w-full h-auto'alt="" />
